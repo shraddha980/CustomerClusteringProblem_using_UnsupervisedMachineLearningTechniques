959911d23368dc094f6724b103f89111f631bdbf
Shraddha Sawant <100741888+shraddha980@users.noreply.github.com>
2023-11-20 23:21:57 +0530

    Update data_validation.py

--- Segmentation/components/data_validation.py
+++ Segmentation/components/data_validation.py
@@ -110,6 +110,7 @@ class DataValidation:
         try:
             logging.info(f"Reading base dataframe")
             base_df = pd.read_csv(r"/config/Customer_Segmentation_/bank-additional-full.csv", sep=';')
+            base_df = base_df.iloc[:5000]
             base_df.replace({"na":np.NAN},inplace=True)
             logging.info(f"Replace na value in base df")
             #base_df has na as null
